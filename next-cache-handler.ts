62a9a58da5c8d2d83bcb1b5ee7fc0833a657b172
Joshua Munn <public@elysee-munn.family>
2024-11-22 10:44:14 +0000

    Rename mjs and ts filesto js

--- next-cache-handler.ts
+++
@@ -1,8 +0,0 @@
-import { cacheHandler } from '@neshca/cache-handler'
-import type { CacheHandler } from '@neshca/cache-handler'
-
-export const handler: CacheHandler = cacheHandler({
-  maxMemorySize: 0.5,
-  enableInMemory: true,
-  enableFileSystem: true,
-})
